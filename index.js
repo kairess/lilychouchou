633844541091b2e8a4c1ddb417ba222ac3166810
Brad <th.lee@ecubelabs.com>
2017-08-08 00:24:35 +0900

    add style

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-var string = "リリイ・シュシュのすべて";
+var messages = [
+    {
+        string: "リリイ・シュシュのすべて",
+        timing: 1000,
+    },
+    {
+        string: "릴리슈슈의 모든것",
+        timing: 3000,
+    }
+];
 
 var unicodeRange = [0x00A1, 0x02B8];
-var braket = "「」";
+var brackets = "「」";
 var offset = 3;
+var typingSpeed = 50;
+var targetId = "title";
+var isDialogue = false;
 
-var newString = new String;
+var outputString = new String;
+var printer;
+var seq;
 
-for(var i=0; i<string.length + offset; i++) {
-    newString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1));
+var printMessage = function(message) {
+    setTimeout(function() {
+        targetObj = document.getElementById(targetId);
+
+        targetObj.innerHTML = "";
+
+        for(var j=0; j<message.string.length + offset; j++) {
+            outputString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1));
+        }
+
+        seq = 0;
+
+        printer = setInterval(function() {
+            targetObj.innerHTML += outputString[seq];
+
+            seq++;
+
+            if(seq >= outputString.length - 2 + offset) {
+                clearInterval(printer);
+
+                targetObj.innerHTML = "《" + message.string + "》";
+            }
+        }, typingSpeed);
+
+    }, message.timing);
+};
+
+for(var i=0; i<messages.length; i++) {
+    printMessage(messages[i]);
 }
-
-var seq = 0;
-
-var printer = setInterval(function() {
-    document.getElementById("title").innerHTML += newString[seq];
-
-    seq++;
-
-    if(seq >= newString.length - 2 + offset) {
-        clearInterval(printer);
-
-        document.getElementById("title").innerHTML = "《" + string + "》";
-    }
-}, 50);
