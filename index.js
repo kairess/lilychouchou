da08e5e4598fbd83c0ea2eb04b70a46230d78a27
Brad <th.lee@ecubelabs.com>
2017-08-07 23:50:20 +0900

    change interval and add settings

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var string = "リリイ・シュシュのすべて";
 
 var unicodeRange = [0x00A1, 0x02B8];
+var braket = "「」";
+var offset = 3;
 
 var newString = new String;
 
-for(var i=0; i<string.length; i++) {
+for(var i=0; i<string.length + offset; i++) {
     newString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1));
 }
 
@@ -15,10 +17,9 @@ var printer = setInterval(function() {
 
     seq++;
 
-    if(seq >= newString.length - 1) {
+    if(seq >= newString.length - 2 + offset) {
         clearInterval(printer);
 
         document.getElementById("title").innerHTML = "《" + string + "》";
-        // alert('stopped at ' + seq);
     }
-}, 100);
+}, 50);
