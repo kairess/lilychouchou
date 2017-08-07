81ced3871f320b0b02b662be4a148eea877115ee
Brad <th.lee@ecubelabs.com>
2017-08-07 23:42:09 +0900

    print unicode sequentially and print original message like a film

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ var string = "リリイ・シュシュのすべて";
 var unicodeRange = [0x00A1, 0x02B8];
 
 var newString = new String;
+
 for(var i=0; i<string.length; i++) {
-    newString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1))
+    newString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1));
 }
 
-document.write(newString);
+var seq = 0;
+
+var printer = setInterval(function() {
+    document.getElementById("title").innerHTML += newString[seq];
+
+    seq++;
+
+    if(seq >= newString.length - 1) {
+        clearInterval(printer);
+
+        document.getElementById("title").innerHTML = "《" + string + "》";
+        // alert('stopped at ' + seq);
+    }
+}, 100);
