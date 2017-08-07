695cbd0699bafe90b03640b6d76246fc41392729
Brad <th.lee@ecubelabs.com>
2017-08-07 23:19:38 +0900

    print random unicode

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.write("asdf");
+var string = "リリイ・シュシュのすべて";
+
+var unicodeRange = [0x00A1, 0x02B8];
+
+var newString = new String;
+for(var i=0; i<string.length; i++) {
+    newString += String.fromCharCode(unicodeRange[0] + Math.random() * (unicodeRange[1]-unicodeRange[0]+1))
+}
+
+document.write(newString);
